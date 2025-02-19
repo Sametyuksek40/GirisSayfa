f0667bd4a75adc097a177b9d7e5aaeec2f2911c6
Sametyuksek40 <samettdew40@gmail.com>
2025-02-19 10:54:11 +0300

    GirisSayfa
    C# ile veri taşıyan yönlendirmeli giriş sayfası

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace LoginFormm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.textKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.textKod2 = new System.Windows.Forms.TextBox();
+            this.textKod1 = new System.Windows.Forms.TextBox();
+            this.textSifre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 49);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Giriş Yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(293, 325);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 20);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Beni Hatırla ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kullanıcı Adı : ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(420, 325);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(96, 16);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Şifremi unuttum";
+            // 
+            // textKullaniciAdi
+            // 
+            this.textKullaniciAdi.Location = new System.Drawing.Point(309, 89);
+            this.textKullaniciAdi.Name = "textKullaniciAdi";
+            this.textKullaniciAdi.Size = new System.Drawing.Size(221, 22);
+            this.textKullaniciAdi.TabIndex = 1;
+            this.textKullaniciAdi.TextChanged += new System.EventHandler(this.textKullaniciAdi_TextChanged);
+            // 
+            // textKod2
+            // 
+            this.textKod2.Enabled = false;
+            this.textKod2.Location = new System.Drawing.Point(430, 185);
+            this.textKod2.Name = "textKod2";
+            this.textKod2.Size = new System.Drawing.Size(100, 22);
+            this.textKod2.TabIndex = 5;
+            this.textKod2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textKod1
+            // 
+            this.textKod1.Location = new System.Drawing.Point(309, 185);
+            this.textKod1.Name = "textKod1";
+            this.textKod1.Size = new System.Drawing.Size(100, 22);
+            this.textKod1.TabIndex = 3;
+            // 
+            // textSifre
+            // 
+            this.textSifre.Location = new System.Drawing.Point(309, 137);
+            this.textSifre.Name = "textSifre";
+            this.textSifre.Size = new System.Drawing.Size(221, 22);
+            this.textSifre.TabIndex = 2;
+            this.textSifre.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Şifre : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(248, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Kod : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.NavajoWhite;
+            this.label4.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(268, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(313, 34);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Admin Login Panel";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textSifre);
+            this.Controls.Add(this.textKod1);
+            this.Controls.Add(this.textKod2);
+            this.Controls.Add(this.textKullaniciAdi);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox textKullaniciAdi;
+        private System.Windows.Forms.TextBox textKod2;
+        private System.Windows.Forms.TextBox textKod1;
+        private System.Windows.Forms.TextBox textSifre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginFormm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        void kodOlustur()
+        {
+            Random rnd = new Random();  
+            int say=rnd.Next(10000,100000);
+            textKod2.Text = say.ToString();
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kodOlustur();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textKullaniciAdi.Text=="Bilgisayar"&&textSifre.Text=="12345"&&textKod1.Text==textKod2.Text)
+            {
+                Form2 frm = new Form2();
+                frm.kullanici=textKullaniciAdi.Text;
+                frm.ShowDialog();
+                this.Hide();
+                
+            }
+            
+        }
+
+        private void textKullaniciAdi_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
